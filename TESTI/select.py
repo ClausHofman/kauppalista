0d36c9be6b7d593c519ca7e06bf6c9644059b8aa
ClausHofman <46158@edu.raseko.fi>
2022-11-21 12:14:13 +0200

    jotain omaa testailua

--- TESTI/select.py
+++ TESTI/select.py
@@ -5,12 +5,19 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 print("Opened database successfully")
 
-cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
-for row in cursor:
-   print("ID = ", row[0])
-   print("NAME = ", row[1])
-   print("ADDRESS = ", row[2])
-   print("SALARY = ", row[3], "\n")
+cursor = conn.execute("SELECT id, name,  age, address, salary from COMPANY")
+# for row in cursor:
+#    print("ID = ", row[0])
+#    print("NAME = ", row[1])
+#    print("ADDRESS = ", row[2])
+#    print("SALARY = ", row[3], "\n")
 
-print("Operation done successfully")
+# print("Operation done successfully")
+
+
+lista = []
+for row in cursor:
+    lista.append(row)
 conn.close()
+
+print(lista)
